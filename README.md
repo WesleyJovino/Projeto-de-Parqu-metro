1e883a92b424ae6c5425993e3aed388927e82e6f
Wesley <wesleyjovinodeavila@gmail.com>
2025-12-15 16:11:27 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Projeto-de-Parqu-metro
---
+++ script.js
@@ -0,0 +1,12 @@
+class parquimetro {
+    constructor(){
+        this.tabelaPrecos = [
+            { valor: 1.00, tempo: 30 },
+            { valor: 1.75, tempo: 60 },
+            { valor: 3.00, tempo: 120 }
+            ];
+        }
+
+        
+    }
+}
